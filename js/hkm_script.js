ae95b060dd3f22d11435ee6d3ad683eae59e4502
M-Hakim <mahmoud.abdelhakim9610@gmail.com>
2020-01-25 05:00:48 +0200

    cleaning up

--- js/hkm_script.js
+++
@@ -1,6 +0,0 @@
-//var _span = document.getElementsByClassName("hkm_slider");
-var _span = document.querySelector(".dd");
-
-_span.addEventListener("click",function test(){
-    console.log("sara");
-});
